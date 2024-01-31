36b20dcd7a43d48dc26c76dbb4e15fcb4cee8e5e
k <kevin.lau.gd@gmail.com>
2024-01-31 21:43:12 +0800

    Create algo_trading_service.h

---
+++ include/xyts/strategy/algo_trading_service.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <memory>
+
+#include "xyts/base/market_data.h"
+#include "xyts/base/trade_msg.h"
+#include "xyts/strategy/strategy_context.h"
+
+namespace xyts::strategy {
+
+using AlgoOrderId = uint64_t;
+
+class AlgoTradingService {
+ public:
+  explicit AlgoTradingService(StrategyContext* ctx);
+
+  ~AlgoTradingService() = default;
+
+  AlgoOrderId AddAlgoOrder(const nlohmann::json& algo_params);
+
+  void CancelAlgoOrder(AlgoOrderId algo_order_id);
+
+  void OnTick(const TickData& tick);
+
+  void OnOrder(const OrderResponse& order);
+
+ private:
+  class Impl;
+  std::unique_ptr<Impl> impl_;
+};
+
+}  // namespace xyts::strategy
