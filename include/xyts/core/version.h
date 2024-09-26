7c116580027a1360c2a5310c3ae668e8da033440
k <xyz@xyz.com>
2024-09-26 17:10:20 +0800

    update from xyts, hash: fe0af2e5b32c42d4d0dc6a9cfd58c46ec34b0453 ref:  (HEAD -> master, local/master)

--- include/xyts/core/version.h
+++ include/xyts/core/version.h
@@ -4,7 +4,7 @@
 
 namespace xyts {
 
-const char* kGitCommitInfo = "hash: 5d2eb5fa3a1c14e7df67b82c684cd9425031f100 date: 2024-09-26 15:55:35 +0800 ref:  (HEAD -> master, local/master)\n";
+const char* kGitCommitInfo = "hash: fe0af2e5b32c42d4d0dc6a9cfd58c46ec34b0453 date: 2024-09-26 17:09:01 +0800 ref:  (HEAD -> master, local/master)\n";
 const char* kCxxCompilerVersion = "g++-11 (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0";
 const char* kCMakeCompilerId = "GNU";
 const char* kCMakeCxxCompiler = "/usr/bin/g++-11";
